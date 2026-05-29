866bf5be30034251c3270e3b8222dd1cc3f97ab4
tontonrabbi <161958815+tontonrabbi@users.noreply.github.com>
2026-05-29 11:13:38 +0200

    29052026

--- les2_oefB.py
+++ les2_oefB.py
@@ -3,3 +3,17 @@ leeftijd = int(input("Wat is je leeftijd? "))
 
 print(f"Hallo {naam}! Volgend jaar ben je {leeftijd + 1} jaar oud.")
 print(naam.upper())
+
+tekst = "HALLO"
+print(tekst.lower())   
+
+
+tekst = "  hallo  "
+print(tekst.strip())  
+
+
+zin = "Ik heb een oud boek."
+print(zin.replace("oud", "nieuw"))  
+
+tekst = "hallo"
+print(len(tekst))   
